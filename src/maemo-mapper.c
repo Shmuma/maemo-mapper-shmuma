14dac8a4e183fd6dfe9327dc8761fb8442ea632a
gnuite <gnuite@gmail.com>
2006-10-21 21:31:03 +0000

    Added nod to Cezary Jackiewicz at his request.
    git-svn-id: svn+ssh://garage/var/lib/gforge/svnroot/maemo-mapper/trunk@32 6c538b50-5814-0410-93ad-8bdf4c0149d1

--- src/maemo-mapper.c
+++ src/maemo-mapper.c
@@ -3,6 +3,8 @@
  *
  * Copyright (C) 2006 John Costigan.
  *
+ * POI and GPS-Info code originally written by Cezary Jackiewicz.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
