31a52a6a4a14c815a2a5c139c00bbe5519522a6c
gnuite <gnuite@gmail.com>
2007-03-02 23:48:32 +0000

    Updated strings, added help, and made other preparations for v1.4.1.
    git-svn-id: svn+ssh://garage/var/lib/gforge/svnroot/maemo-mapper/trunk@85 6c538b50-5814-0410-93ad-8bdf4c0149d1

--- po/update-po.sh
+++ po/update-po.sh
@@ -4,5 +4,8 @@ xgettext --keyword=_ -C -s -o template.pot `dirname $0`/../src/maemo-mapper.c
 
 for FILE in `dirname $0`/*.po
 do
-	msgmerge -q -s -U $FILE template.pot
+    if test ! -L "$FILE"
+    then
+        msgmerge -q -s -U $FILE template.pot
+    fi
 done
