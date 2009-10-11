512ddef69900190a5ce56f050a03017ab6217bbf
Alberto Mardegan <mardy@users.sourceforge.net>
2009-10-11 12:29:05 +0300

    Add new object: MapController

---
+++ src/controller.c
@@ -0,0 +1,85 @@
+/* vi: set et sw=4 ts=4 cino=t0,(0: */
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * Copyright (C) 2009 Alberto Mardegan.
+ *
+ * This file is part of Maemo Mapper.
+ *
+ * Maemo Mapper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Maemo Mapper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Maemo Mapper.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifdef HAVE_CONFIG_H
+#   include "config.h"
+#endif
+#include "controller.h"
+
+#include "data.h"
+#include "defines.h"
+#include "screen.h"
+
+struct _MapControllerPrivate
+{
+    MapScreen *screen;
+
+    guint is_disposed : 1;
+};
+
+G_DEFINE_TYPE(MapController, map_controller, G_TYPE_OBJECT);
+
+#define MAP_CONTROLLER_PRIV(controller) (MAP_CONTROLLER(controller)->priv)
+
+static void
+map_controller_dispose(GObject *object)
+{
+    MapControllerPrivate *priv = MAP_CONTROLLER_PRIV(object);
+
+    if (priv->is_disposed)
+        return;
+
+    priv->is_disposed = TRUE;
+
+    G_OBJECT_CLASS(map_controller_parent_class)->dispose(object);
+}
+
+static void
+map_controller_init(MapController *controller)
+{
+    MapControllerPrivate *priv;
+
+    priv = G_TYPE_INSTANCE_GET_PRIVATE(controller, MAP_TYPE_CONTROLLER,
+                                       MapControllerPrivate);
+    controller->priv = priv;
+
+    priv->screen = g_object_new(MAP_TYPE_SCREEN, NULL);
+    map_screen_show_compass(priv->screen, _show_comprose);
+    map_screen_show_scale(priv->screen, _show_scale);
+    map_screen_show_zoom_box(priv->screen, _show_zoomlevel);
+}
+
+static void
+map_controller_class_init(MapControllerClass *klass)
+{
+    GObjectClass *object_class = G_OBJECT_CLASS(klass);
+
+    g_type_class_add_private(object_class, sizeof(MapControllerPrivate));
+
+    object_class->dispose = map_controller_dispose;
+}
+
+GtkWidget *
+map_controller_get_screen_widget(MapController *self)
+{
+    g_return_val_if_fail(MAP_IS_CONTROLLER(self), NULL);
+    return (GtkWidget *)self->priv->screen;
+}
+
---
+++ src/controller.h
@@ -0,0 +1,60 @@
+/* vi: set et sw=4 ts=4 cino=t0,(0: */
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * Copyright (C) 2009 Alberto Mardegan.
+ *
+ * This file is part of Maemo Mapper.
+ *
+ * Maemo Mapper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Maemo Mapper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Maemo Mapper.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MAP_CONTROLLER_H
+#define MAP_CONTROLLER_H
+
+#include <glib.h>
+#include <glib-object.h>
+#include <gtk/gtk.h>
+ 
+G_BEGIN_DECLS
+
+#define MAP_TYPE_CONTROLLER         (map_controller_get_type ())
+#define MAP_CONTROLLER(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), MAP_TYPE_CONTROLLER, MapController))
+#define MAP_CONTROLLER_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), MAP_TYPE_CONTROLLER, MapControllerClass))
+#define MAP_IS_CONTROLLER(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), MAP_TYPE_CONTROLLER))
+#define MAP_IS_CONTROLLER_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), MAP_TYPE_CONTROLLER))
+#define MAP_CONTROLLER_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), MAP_TYPE_CONTROLLER, MapControllerClass))
+
+typedef struct _MapController MapController;
+typedef struct _MapControllerPrivate MapControllerPrivate;
+typedef struct _MapControllerClass MapControllerClass;
+
+
+struct _MapController
+{
+    GObject parent;
+
+    MapControllerPrivate *priv;
+};
+
+struct _MapControllerClass
+{
+    GObjectClass parent_class;
+};
+
+GType map_controller_get_type (void);
+
+GtkWidget *map_controller_get_screen_widget(MapController *self);
+
+G_END_DECLS
+#endif /* MAP_CONTROLLER_H */
