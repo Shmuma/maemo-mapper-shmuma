076fba191a1d96f33b9ffcfb53be2965ad22eead
Max Lapan <max.lapan@gmail.com>
2009-12-13 10:48:39 +0300

    Fix deb package build issues.

--- dpkg-build.sh
+++ dpkg-build.sh
@@ -5,18 +5,28 @@
 # On an N800 (-mcpu=arm1136jf-s), the version is suffixed with "-3".
 # On a 770 (-mcpu=arm926ej-s), the version is suffixed with "-2".
 # In all other cases, the version is suffixed with "-1".
-# 
+#
 
 CHANGELOG=`dirname $0`/debian/changelog
 
 # Test whether we're running Chinook or a "legacy" Maemo.
-MAEMO_VERSION=`pkg-config libosso --modversion | cut -d . -f 1`
-if [ "x$MAEMO_VERSION" = "x1" ]
-then
-    VERSION_SUFFIX=-os2006-os2007
-else
-    VERSION_SUFFIX=-os2008
-fi
+MAEMO_VERSION=`cat /etc/maemo_version | cut -d ' ' -f 1`
+case $MAEMO_VERSION in
+    4.*)
+        VERSION_SUFFIX=-os2008
+        echo 4.x
+        break
+        ;;
+    5.*)
+        VERSION_SUFFIX=-alpha1-maemo5
+        echo 5.x
+        break
+        ;;
+    *)
+        VERSION_SUFFIX=-os2006-os2007
+        echo all overs
+        break;;
+esac
 
 sed -i "1,1s/(\([0-9.][0-9.]*\)[-a-zA-Z0-9]*)/(\1$VERSION_SUFFIX)/" $CHANGELOG
 
